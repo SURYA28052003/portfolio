671116620e5bf552aa3f28c4679925dfea11ab78
SURYA28052003 <93878879+SURYA28052003@users.noreply.github.com>
2024-01-29 17:35:48 +0530

    final

--- form_app/forms.py
+++ form_app/forms.py
@@ -6,3 +6,9 @@ class UserInfoForm(forms.ModelForm):
     class Meta:
         model = UserInfo
         fields = ['name', 'email', 'phone', 'message']
+        
+    # def __init__(self, *args, **kwargs):
+    #     super(UserInfoForm, self).__init__(*args, **kwargs)
+
+    # for field_name in self.fields:
+    #         self.fields[field_name].widget.attrs['autocomplete'] = field_name
